14a5a5a8f34bb1e4e672a7a2adae6fa2af10799c
Andrii Savchuk <andriksr@gmail.com>
2017-05-30 19:50:06 +0300

    added omdb.js

--- routes/search.js
+++ routes/search.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 const path = require('path');
+const omdb = require('../lib/omdb');
 
 
 function search(req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
+
+  omdb.get('avengers', (error, movie) => {
+    if (error) throw error;
+
+    console.log(movie);
+  });
 
   const stream = fs.createReadStream(path.resolve('public', 'movie.html')); // public/css/app.css
 
