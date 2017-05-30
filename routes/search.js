abf62f95dc8f0da5126c0e28c09c66c13628cba7
Andrii Savchuk <andriksr@gmail.com>
2017-05-30 20:06:13 +0300

    fixed

--- routes/search.js
+++ routes/search.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
 const path = require('path');
+const url = require('url');
 const omdb = require('../lib/omdb');
 
 
+
 function search(req, res) {
+  const parseUrl = url.parse(req.url, true); // if true then we get object, without -> String
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
 
-  omdb.get('avengers', (error, movie) => {
+  omdb.get(title, (error, movie) => {
     if (error) throw error;
 
     console.log(movie);
